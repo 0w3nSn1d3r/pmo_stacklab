cb0de5de1af6ccaec6f19e4d562e44912eed6846
Owen <owenjohnston1229@gmail.com>
2026-01-25 12:02:27 -0800

    chore(core): finish docstring and commenting for calc_final_img func

--- pmo_stacklab/src/core.py
+++ pmo_stacklab/src/core.py
@@ -15,33 +15,50 @@ def calc_final_img(
         post_processer: PostProcess
 ) -> CCDData:
     """
-    Docstring for gen_final_img
+    Extracts data from folder using parse_data_folder(folder_path)
+    then applies complete configured stacking pipeline
+    to produce the final image
 
-    :param folder_path: Description
+    :param folder_path: path to structured folder
+    containing all data; must be of GUI-generated
+    structure, with subfolders per type and filter
     :type folder_path: Path
 
-    :param calibrate: Description
+    :param calibrater: instance of Calibrate class
+    with configured calibration methods passed;
+    calibrate() method is applied to data
     :type calibrate: Calibrate
 
-    :param register: Description
-    :type register: Register
+    :param reprojecter: instance of Reproject class
+    with configured reprojection methods passed;
+    reproject() method is applied to data
+    :type reproject: Register
 
-    :param stack: Description
+    :param stacker: instance of Stack class
+    with configured stacking methods passed;
+    stack() method is applied to data
     :type stack: Stack
 
-    :param post_process: Description
+    :param post_processer: instance of PostProcess class
+    with configured post-processing methods passed;
+    post_process() method is applied to data
     :type post_process: PostProcess
 
-    :return: Description
+    :return: contains image pixel-value data
+    for complete calibrated, reprojected, stacked,
+    and post-processed image; also contains
+    relevant metadata, such as WCS and units
     :rtype: CCDData
     """
 
+    # Extract CCDData from folder
     all_data = parse_data_folder(folder_path)
     lights = all_data['lights']
     darks = all_data['darks']
     biases = all_data['biases']
     flats = all_data['flats']
 
+    # Apply stacking pipeline
     calibrated = calibrater.calibrate(lights, darks, biases, flats)
     reprojected = reprojecter.reproject(calibrated)
     stacked = stacker.stack(reprojected)
