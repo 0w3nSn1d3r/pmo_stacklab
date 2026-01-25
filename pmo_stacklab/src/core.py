2160e793e2a11a878a492ab6cd75285c70934f85
Owen <owenjohnston1229@gmail.com>
2026-01-25 10:08:45 -0800

    feature(core): implement calc_final_img func to orchestrate complete processing pipeline

--- pmo_stacklab/src/core.py
+++ pmo_stacklab/src/core.py
@@ -1,13 +1,18 @@
 from astropy.nddata import CCDData
 from pathlib import Path
+from modules.calibration.calibrate import Calibrate
+from modules.reprojection.reproject import Reproject
+from modules.stacking.stack import Stack
+from modules.post_processing.post_process import PostProcess
+from utils.folder2fits import parse_data_folder
 
 
-def gen_final_img(
+def calc_final_img(
         folder_path: Path,
-        calibrate: Calibrate,
-        register: Register,
-        stack: Stack,
-        post_process: PostProcess
+        calibrater: Calibrate,
+        reprojecter: Reproject,
+        stacker: Stack,
+        post_processer: PostProcess
 ) -> CCDData:
     """
     Docstring for gen_final_img
@@ -30,4 +35,16 @@ def gen_final_img(
     :return: Description
     :rtype: CCDData
     """
-    return
+
+    all_data = parse_data_folder(folder_path)
+    lights = all_data['light']
+    darks = all_data['dark']
+    biases = all_data['bias']
+    flats = all_data['flat']
+
+    calibrated = calibrater.calibrate(lights, darks, biases, flats)
+    reprojected = reprojecter.reproject(calibrated)
+    stacked = stacker.stack(reprojected)
+    final_img = post_processer.post_process(stacked)
+
+    return final_img
