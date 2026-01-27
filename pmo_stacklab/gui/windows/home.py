f215354e9079a52618c01ca19390e2c71e3082fa
Owen <owenjohnston1229@gmail.com>
2026-01-26 18:53:59 -0800

    fix(home page): add Post Process tab; add primary home screen frame to introduce StackLab

--- pmo_stacklab/gui/windows/home.py
+++ pmo_stacklab/gui/windows/home.py
@@ -1,19 +1,25 @@
 from tkinter import ttk, filedialog
 import tkinter as tk
-from frames.home import UploadFrame, CalibrateFrame, StackFrame
+import frames.home as home
 
 
 class HomeWindow:
     def __init__(self, root):
-        root.title('PMO StackLab | Home')
+        root.title('UO | PMO StackLab')
 
         # One phase of processing per tab for clarity
         tabs = ttk.Notebook(root)
 
-        uploadtab = UploadFrame(root)
-        calibratetab = CalibrateFrame(root)
-        stacktab = StackFrame(root)
+        upload_tab = home.UploadFrame(root)
+        calibrate_tab = home.CalibrateFrame(root)
+        stack_tab = home.StackFrame(root)
+        postprocess_tab = home.PostProcessFrame(root)
 
-        tabs.add(uploadtab, text='Upload')
-        tabs.add(calibratetab, text='Calibrate')
-        tabs.add(stacktab, text='Stack')
+        tabs.add(upload_tab, text='Upload')
+        tabs.add(calibrate_tab, text='Calibrate')
+        tabs.add(stack_tab, text='Stack')
+        tabs.add(postprocess_tab, text='Post Process')
+
+        # Home frame for introduction to StackLab
+        home_frame = home.HomeFrame(root)
+        home_frame.pack()
---
+++ pmo_stacklab/main.py
@@ -0,0 +1,6 @@
+from tkinter import ttk
+import tkinter as tk
+from gui.windows.home import HomeWindow
+
+root = tk.root()
+HomeWindow(root)
