98d74a9aa3fec5e1464e2a68ea3f7fa64056565f
Owen <owenjohnston1229@gmail.com>
2026-01-31 09:40:12 -0800

    chore: outline HomeFrame widgets

--- pmo_stacklab/gui/frames/home_tab/main_home.py
+++ pmo_stacklab/gui/frames/home_tab/main_home.py
@@ -5,3 +5,9 @@ import tkinter as tk
 class HomeFrame(ttk.Frame):
     def __init__(self, root):
         super().__init__(root)
+        mainframe = ttk.Frame(root)
+
+        intro = tk.Text(mainframe, width=100, height=150)
+        intro.insert('HOW TO USE PMO STACKLAB HERE')
+
+        intro.pack()
