5e6583b92c867f02a70bdd25a03f6d23951e905c
Owen <owenjohnston1229@gmail.com>
2026-02-02 20:37:59 -0800

    chore(gui): implement basic styling; standardize page layout

--- pmo_stacklab/src/modules/post_processing/post_process.py
+++ pmo_stacklab/src/modules/post_processing/post_process.py
@@ -9,13 +9,11 @@ class PostProcess:
             color_map: function,
             stretch: function,
             intensity_scaling: function,
-            channel_combination: function
     ):
         self.background_modeling = background_modeling
         self.color_map = color_map
         self.stretch = stretch
         self.intensity_scaling = intensity_scaling
-        self.channel_combination = channel_combination
 
     # TO-DO
     def post_process(self, data: np.ndarray) -> CCDData:
